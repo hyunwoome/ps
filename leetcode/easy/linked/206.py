f831642f97db5bc3f97f68bfba652b959869cc27
Lim Hyunwoo <hyunwoome@icloud.com>
2022-01-17 13:07:36 +0900

    solve: LeetCode 21

--- leetcode/easy/linked/206.py
+++ leetcode/easy/linked/206.py
@@ -1,12 +1,37 @@
+# 206. Reverse Linked List
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def print_all(self):
+        while self:
+            print(self.val, end=' ')
+            self = self.next
+
+
 class Solution:
-    def reverseList(self, head):
-        curr = head
+    def reverse_list(self, head):
+        cur = head
         prev = None
 
-        while curr is not None:
-            next = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next
+        # cur이 None이 아닐 때 까지
+        while cur is not None:
+            next = cur.next
+            cur.next = prev
+            prev = cur
+            cur = next
 
         return prev
+
+
+list5 = ListNode(5)
+list4 = ListNode(4, list5)
+list3 = ListNode(3, list4)
+list2 = ListNode(2, list3)
+list1 = ListNode(1, list2)
+print(list1.print_all())
+
+sol = Solution()
+result = sol.reverse_list(list1)
+result.print_all()  # 5 4 3 2 1
