4e4ca7c67d576b61f118fea4b1913981ba3e39c8
Lim Hyunwoo <hyunwoome@icloud.com>
2022-01-17 21:38:25 +0900

    solve : LeetCode 819

---
+++ leetcode/easy/string/819.py
@@ -0,0 +1,24 @@
+import re
+
+
+class Solution:
+    def most_common_word(self, paragraph: str, banned):
+        dic = {}
+        banned = set(banned)
+        for c in "!?',;.":
+            paragraph = paragraph.replace(c, " ")
+        paragraph = paragraph.lower().split()
+
+        for word in paragraph:
+            if word not in banned:
+                if word in dic:
+                    dic[word] += 1
+                else:
+                    dic[word] = 1
+        return max(dic, key=dic.get)
+
+
+p = "Bob hit a ball, the hit BALL flew far after it was hit."
+b = ["hit"]
+sol = Solution()
+print(sol.most_common_word(p, b))
