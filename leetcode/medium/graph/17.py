ee99021e9a29d9c8b6913ea7e14b63d3cbe09c03
Lim Hyunwoo <hyunwoome@icloud.com>
2022-01-23 11:36:11 +0900

    solve : LeetCode 17

--- leetcode/medium/graph/17.py
+++ leetcode/medium/graph/17.py
@@ -1,21 +1,25 @@
 class Solution:
     def letterCombinations(self, digits):
+
+        def dfs(index, path):
+            # 종료조건
+            if len(path) == len(digits):
+                result.append(path)
+                return
+
+            for i in range(index, len(digits)):
+                for j in dic[digits[i]]:
+                    dfs(i + 1, path + j)
+
         dic = {'2': 'abc', '3': 'def',
                '4': 'ghi', '5': 'jkl', '6': 'mno',
                '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
-        if len(digits) == 0:
-            return []
 
-        if len(digits) == 1:
-            return list(dic[digits[0]])
-
+        result = []
+        dfs(0, '')
+        return result
 
 
-
-
-
-
-
-digits = '23'
+d = '23'
 sol = Solution()
-print(sol.letterCombinations(digits))
+print(sol.letterCombinations(d))
