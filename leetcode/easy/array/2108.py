0f3953e30cb10ad4a990e594431bb26c3e0946f7
Lim Hyunwoo <hyunwoome@icloud.com>
2022-04-21 12:47:04 +0900

    solve: LeetCode 2108 discussion

--- leetcode/easy/array/2108.py
+++ leetcode/easy/array/2108.py
@@ -1,32 +1,45 @@
 from typing import List
 
 
+# class Solution:
+#     def firstPalindrome(self, words: List[str]) -> str:
+#
+#         def is_palindrome(word: str):
+#             if len(word) % 2 == 0:
+#                 _start_word = word[:len(word) // 2]
+#                 _end_word = word[len(word) // 2:][::-1]
+#
+#                 if _start_word == _end_word:
+#                     return True
+#
+#             else:
+#                 _start_word = word[:len(word) // 2]
+#                 _end_word = word[len(word) // 2 + 1:][::-1]
+#
+#                 if _start_word == _end_word:
+#                     return True
+#
+#             return False
+#
+#         for word in words:
+#             if is_palindrome(word):
+#                 return word
+#
+#         return ""
+#
+#
+# words = ["abc", "car", "ada", "racecar", "cool"]
+# sol = Solution()
+# print(sol.firstPalindrome(words))
+
+
 class Solution:
     def firstPalindrome(self, words: List[str]) -> str:
-
-        def is_palindrome(word: str):
-            if len(word) % 2 == 0:
-                _start_word = word[:len(word) // 2]
-                _end_word = word[len(word) // 2:][::-1]
-
-                if _start_word == _end_word:
-                    return True
-
-            else:
-                _start_word = word[:len(word) // 2]
-                _end_word = word[len(word) // 2 + 1:][::-1]
-
-                if _start_word == _end_word:
-                    return True
-
-            return False
-
         for word in words:
-            if is_palindrome(word):
+            if word == word[::-1]:
                 return word
 
         return ""
-
 
 words = ["abc", "car", "ada", "racecar", "cool"]
 sol = Solution()
