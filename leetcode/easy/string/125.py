62ac2cd093e0ff6cd3bae73382e13e6365026bba
Lim Hyunwoo <hyunwoome@icloud.com>
2022-04-28 13:28:58 +0900

    solve: LeetCode 125

--- leetcode/easy/string/125.py
+++ leetcode/easy/string/125.py
@@ -1,30 +1,15 @@
-# 1. 파이썬 문자열 기능 사용
-# class Solution:
-#     def is_palindrome(self, s):
-#         remove_space = s.replace(' ', '')  # 1. 공백 제거
-#         remove_non_alpha = ''.join(filter(str.isalnum, remove_space)) # 2. 특수문자 제거
-#         to_lowercase = remove_non_alpha.lower()  # 3. 소문자로 변경
-#         return to_lowercase == to_lowercase[::-1]
-#
-# s = 'A man, a plan, a canal: Panama'
-# sol = Solution()
-# print(sol.is_palindrome(s))
-# Runtime: 70ms, faster than 27.07%
-
-
-
-# 2. 정규식을 사용한 문제 풀이
 import re
 
 
-class Solution():
-    def is_palindrome(self, s):
-        s = s.lower()  # 소문자 변경
-        s = re.sub('[^a-z0-9]', '', s)  # a-z, 0-9까지를 제외한 나머지 문자열은 제거
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = re.sub('[^a-z0-9]', '', s)
+
         return s == s[::-1]
 
 
-s = 'A man, a plan, a canal: Panama'
+
 sol = Solution()
-print(sol.is_palindrome(s))
-# Runtime: 36 ms, faster than 96.36%
+_s = "A man, a plan, a canal: Panama"
+print(sol.isPalindrome(_s))
