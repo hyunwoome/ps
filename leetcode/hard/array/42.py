441209cf708fc6876f504218dce55fff997b0684
Lim Hyunwoo <hyunwoome@icloud.com>
2022-05-02 23:01:24 +0900

    solve: LeetCode 42

--- leetcode/hard/array/42.py
+++ leetcode/hard/array/42.py
@@ -1,25 +1,33 @@
-class Solution:
-    def trap(self, height):
-        answer = 0
+from typing import List
 
-        left, right = 0, len(height) - 1
-        left_max, right_max = height[left], height[right]
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+
+        volume = 0
+
+        left = 0
+        right = len(height) - 1
+
+        left_max = height[left]
+        right_max = height[right]
 
         while left < right:
-            left_max = max(left_max, height[left])
-            right_max = max(right_max, height[right])
+            left_max = max(height[left], left_max)
+            right_max = max(height[right], right_max)
 
             if left_max <= right_max:
-                answer += left_max - height[left]
+                volume += left_max - height[left]
                 left += 1
             else:
-                answer += right_max - height[right]
+                volume += right_max - height[right]
                 right -= 1
 
-        return answer
+        return volume
 
 
-
-height = [4, 2, 3]
 sol = Solution()
-print(sol.trap(height))
+_height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
+print(sol.trap(_height))
