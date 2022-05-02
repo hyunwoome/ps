7da42a36d676a1fd5c98857310a4ad4e2d44f429
Lim Hyunwoo <hyunwoome@icloud.com>
2022-05-02 21:32:44 +0900

    solve: LeetCode 1

--- leetcode/easy/array/1.py
+++ leetcode/easy/array/1.py
@@ -1,17 +1,17 @@
 from typing import List
 
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dic = {}
+        for i, v in enumerate(nums):
+            complement = target - v
 
-        for index, value in enumerate(nums):
-            if target - value in dic:
-                return [dic[target - value], index]
-            dic[value] = index
+            if complement in nums[i + 1:]:
+                return [nums.index(v), nums[i + 1:].index(complement) + (i + 1)]
+
 
 
 sol = Solution()
-_nums = [2, 7, 11, 15]
-_target = 9
+_nums = [1, 3, 6, 8, 9, 12, 15, 43, 53]
+_target = 61
 print(sol.twoSum(_nums, _target))
-
