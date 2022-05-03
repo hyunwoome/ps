373caac9e3b4a34617a999f69827a327079b4fec
Lim Hyunwoo <hyunwoome@icloud.com>
2022-05-03 16:58:35 +0900

    solve: LeetCode 238

--- leetcode/medium/array/238.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def productExceptSelf(self, nums):
-
-
-
-nums = [1, 2, 3, 4]
-sol = Solution()
-print(sol.productExceptSelf(nums))
---
+++ leetcode/medium/array/Leet238.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        result = []
+        p = 1
+
+        for i in range(len(nums)):
+            result.append(p)
+            p *= nums[i]
+
+        p = 1
+
+        for i in range(len(nums) - 1, -1, -1):
+            result[i] *= p
+            p *= nums[i]
+
+        return result
+
+
+sol = Solution()
+_nums = [1, 2, 3, 4]
+print(sol.productExceptSelf(_nums))
